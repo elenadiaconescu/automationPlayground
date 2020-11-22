f01ef07fcd35e69fbb2b83415a25cff88d89753d
Elena Diaconescu <elena.86.diaconescu@gmail.com>
2020-11-22 20:52:29 +0100

    switching

---
+++ udemy/cypress/integration/02-select-elements/start.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+it('visits todoMVC app', () => {
+
+  cy
+    .visit('localhost:3000');
+
+  // select via tag
+  // cy
+  //   .get('input'); 
+
+  // select via id
+
+  // select via class
+
+  // select via attribute
+
+  // select via relation
+
+});
